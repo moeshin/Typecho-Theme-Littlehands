81024d1422917f1028630bd3d20b18e2f155dd30
MoeShin <hi@moeshin.com>
2021-10-06 17:29:34 +0800

    下载 CDN map 映射文件

--- assets/cdn/download.py
+++ assets/cdn/download.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 # coding=utf-8
-
+import re
 import urllib.request
+import urllib.error
 import os
 
-cdn = 'https://cdn.staticfile.org/'
+CDN_URL = 'https://cdn.staticfile.org/'
+RE_SOURCE = re.compile(r'(?:/\*# sourceMappingURL=(.+?) \*/|//# sourceMappingURL=(.+?))$')
 
-with open('urls.txt', 'r') as urls:
-    for line in urls:
-        path = line.strip()
-        url = cdn + path
-        path = 'local/' + path
-        dir = os.path.dirname(path)
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        print(url + ' => ' + path)
-        urllib.request.urlretrieve(url, path)
+
+def download(name):
+    url = CDN_URL + name
+    path = 'local/' + name
+    d = os.path.dirname(path)
+    if not os.path.exists(d):
+        os.makedirs(d)
+    print(url + ' => ' + path)
+    urllib.request.urlretrieve(url, path)
+    return path
+
+
+def main():
+    with open('urls.txt', encoding='utf-8') as urls:
+        for line in urls:
+            name = line.strip()
+            path = download(name)
+            with open(path, encoding='utf-8') as f:
+                search = RE_SOURCE.search(f.read())
+                if search is None:
+                    continue
+                source = search.group(1)
+                if source is None:
+                    source = search.group(2)
+                # print(source)
+                name = os.path.dirname(name) + '/' + source
+                print(name)
+                download(name)
+
+
+if __name__ == '__main__':
+    main()
